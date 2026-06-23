b982aef84e8ce2c4283b13f20b9546ba3a814820
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-06-24 00:41:54 +0530

    Time: 39 ms (61.64%), Space: 85.7 MB (92.72%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size()==0){return 0;}
+        sort(nums.begin() , nums.end());
+        int maxdiff = 0;
+        for(int i = 1 ; i<nums.size() ; i++){
+            maxdiff = max(maxdiff , nums[i]-nums[i-1]);
+
+        }
+        return maxdiff;
+        
+    }
+};
