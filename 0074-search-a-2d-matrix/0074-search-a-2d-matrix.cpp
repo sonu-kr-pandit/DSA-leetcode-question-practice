a565e18a8d7cf7f1bb0d10ab86a176f4e0c5d1d8
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-06-24 14:46:35 +0530

    Time: 0 ms (100%), Space: 13.2 MB (78.59%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int tgt) {
+        int i =0;
+        int n =  matrix[0].size();
+        int j = n-1;
+        while(j>=0 && i< matrix.size()){
+            if(matrix[i][j]==tgt){
+                return true;
+            }else if(tgt<matrix[i][j]){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return false;
+        
+    }
+};
