2f0476de8004e6f51beb32e43cedf65ec7aa5fd1
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-06-26 01:19:24 +0530

    Time: 0 ms (100%), Space: 10 MB (91.98%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        // vector<vector<int>> ans(n , vector<int> (n));
+        // vector<vector<int>> ans(n , vector<int> (n));                     //  method  1 good tc but bad sc
         // for(int i =  0 ; i<n ; i++){               // transpose
         //     for(int j = 0 ; j< n ; j++){
         //         ans[j][i]= matrix[i][j];
@@ -23,20 +23,27 @@ public:
         //     }
         // }
 
-        for(int i = 0; i< n ; i++){
-            for(int j = i+1 ; j <n ; j++){
-                swap(matrix[i][j] , matrix[j][i]);
-            }
+        // for(int i = 0; i< n ; i++){                                // good tc but still bad sc
+        //     for(int j = i+1 ; j <n ; j++){
+        //         swap(matrix[i][j] , matrix[j][i]);
+        //     }
+        // }
+        // for(int i = 0 ; i<n ; i++){               // reverse the row
+        //  int st =0;
+        //  int end = n-1;
+        //  while(st<end){
+        //      swap(matrix[i][st] , matrix[i][end]);
+        //      st++;
+        //      end--;
+        //  }
+        // }
+        for(int i = 0; i< n ; i++){                        // good tc and good sc
+         for(int j = i+1 ; j <n ; j++){
+             swap(matrix[i][j] , matrix[j][i]);
+         }}
+        
+        for(int i = 0 ; i<n ; i++){               // reverse the row
+            reverse(matrix[i].begin() , matrix[i].end());
         }
-           for(int i = 0 ; i<n ; i++){               // reverse the row
-            int st =0;
-            int end = n-1;
-            while(st<end){
-                swap(matrix[i][st] , matrix[i][end]);
-                st++;
-                end--;
-            }
-        } 
-
     }
 };
