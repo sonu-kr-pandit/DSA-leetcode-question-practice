6db563965e77efaff7d9404e7d7ae30b162adc15
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-06-26 01:14:39 +0530

    Time: 0 ms (100%), Space: 10.3 MB (7.87%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,27 +2,41 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> ans(n , vector<int> (n));
-        for(int i =  0 ; i<n ; i++){
-            for(int j = 0 ; j< n ; j++){
-                ans[j][i]= matrix[i][j];
+        // vector<vector<int>> ans(n , vector<int> (n));
+        // for(int i =  0 ; i<n ; i++){               // transpose
+        //     for(int j = 0 ; j< n ; j++){
+        //         ans[j][i]= matrix[i][j];
+        //     }
+        // }
+        // for(int i = 0 ; i<n ; i++){               // reverse the row
+        //     int st =0;
+        //     int end = n-1;
+        //     while(st<end){
+        //         swap(ans[i][st] , ans[i][end]);
+        //         st++;
+        //         end--;
+        //     }
+        // } 
+        // for(int i =  0 ; i<n ; i++){               // assign the valu
+        //     for(int j = 0 ; j< n ; j++){
+        //         matrix[i][j] = ans[i][j];
+        //     }
+        // }
+
+        for(int i = 0; i< n ; i++){
+            for(int j = i+1 ; j <n ; j++){
+                swap(matrix[i][j] , matrix[j][i]);
             }
         }
-       
-        for(int i = 0 ; i<n ; i++){
+           for(int i = 0 ; i<n ; i++){               // reverse the row
             int st =0;
             int end = n-1;
             while(st<end){
-                swap(ans[i][st] , ans[i][end]);
+                swap(matrix[i][st] , matrix[i][end]);
                 st++;
                 end--;
             }
         } 
-        for(int i =  0 ; i<n ; i++){
-            for(int j = 0 ; j< n ; j++){
-                matrix[i][j] = ans[i][j];
-            }
-        }
 
     }
 };
