00aaf16b75fa295c8006e3022245e47255583e51
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-06-27 16:37:52 +0530

    Time: 0 ms (100%), Space: 8.3 MB (16.06%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     string toLowerCase(string& s) {
         for(int i = 0; i<s.size() ; i++){
-            // if(s[i]-'a' >= 0 ){
-            //     int chr = s[i]-'A'-3;
-            //     s[i] = chr+'a';
-            // }
 
-
-            char  ch =s[i];
-            if(ch>='A' && ch<= 'Z'){s[i] = ch - 'A' +'a';}
-            else{
-                continue;
+            int ch = s[i];
+            if(ch>=65 && ch<=90 ){
+                int chr = s[i]-'A'-3;
+                s[i] = ch-'A' +'a';
             }
+
+
+            // char  ch =s[i];
+            // if(ch>='A' && ch<= 'Z'){s[i] = ch - 'A' +'a';}
+            // else{
+            //     continue;
+            // }
         }
         return s;
         
