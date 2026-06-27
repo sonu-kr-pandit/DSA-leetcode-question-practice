a48111beceec126dbb4217bac4af027712e6bc76
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-06-27 23:57:19 +0530

    Time: 0 ms (100%), Space: 9.7 MB (94.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
     bool isPalindrome(string& s) {
-        for(int i =0 ; i<s.size() ; i++){
-            if(!isalnum(s[i])){
-                s.erase(i , 1);
-                i--;
-            }
-        }
-        for(int i = 0; i<s.size() ; i++){                
-            if(s[i]>='A' && s[i]<= 'Z'){s[i] = s[i] - 'A' +'a';}
-        }
-        int st = 0;
-        int end = s.size() - 1;
-        while(st<=end){
-            if(s[st]!=s[end]){return false;}
-            st++;
-            end--;
-        }
-
-
-        //  int left = 0, right = s.size() - 1;
-        
-        // while (left < right) {
-        //     // skip non-alphanumeric from left
-        //     while (left < right && !isalnum(s[left]))
-        //         left++;
-            
-        //     // skip non-alphanumeric from right
-        //     while (left < right && !isalnum(s[right]))
-        //         right--;
-            
-        //     // compare ignoring case
-        //     if (tolower(s[left]) != tolower(s[right]))
-        //         return false;
-            
-        //     left++;
-        //     right--;
+        // for(int i =0 ; i<s.size() ; i++){
+        //     if(!isalnum(s[i])){
+        //         s.erase(i , 1);
+        //         i--;
+        //     }
         // }
+        // for(int i = 0; i<s.size() ; i++){                
+        //     if(s[i]>='A' && s[i]<= 'Z'){s[i] = s[i] - 'A' +'a';}
+        // }
+        // int st = 0;
+        // int end = s.size() - 1;
+        // while(st<=end){
+        //     if(s[st]!=s[end]){return false;}
+        //     st++;
+        //     end--;
+        // }
+
+
+         int left = 0, right = s.size() - 1;
+        while (left < right) {
+            while (left < right && !isalnum(s[left])){ left++;}
+            while (left < right && !isalnum(s[right])){right--;}
+            if (tolower(s[left]) != tolower(s[right])){
+                return false;}
+            left++;
+            right--;
+        }
         return true;
     }
 };
