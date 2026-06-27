e0ec35db465eb11ce26e2e3cc3ed590ffbe97c45
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-06-27 23:35:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.6%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -3,18 +3,18 @@ public:
     string toLowerCase(string& s) {
         for(int i = 0; i<s.size() ; i++){
 
-            // int ch = s[i];        // TC = O(N); BUT THE SC ISN'T GOOD IN'S CONSIST INEGER 4BYTE
-            // if(ch>=65 && ch<=90 ){
-            //     int chr = s[i]-'A'-3;
-            //     s[i] = ch-'A' +'a';
-            // }
-
-
-            char  ch =s[i];                     // TC = O(N); BUT THE SC ISN'T GOOD IN'S CONSIST CHAR 1 BYTE
-            if(ch>='A' && ch<= 'Z'){s[i] = ch - 'A' +'a';}
-            else{
-                continue;
+            int ch = s[i];        // TC = O(N); BUT THE SC ISN'T GOOD IN'S CONSIST INEGER 4BYTE
+            if(ch>=65 && ch<=90 ){
+                int chr = s[i]-'A'-3;
+                s[i] = ch-'A' +'a';
             }
+
+
+            // char  ch =s[i];                     // TC = O(N); BUT THE SC ISN'T GOOD IN'S CONSIST CHAR 1 BYTE
+            // if(ch>='A' && ch<= 'Z'){s[i] = ch - 'A' +'a';}
+            // else{
+            //     continue;
+            // }
         }
         return s;
         
