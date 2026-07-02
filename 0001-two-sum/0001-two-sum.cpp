5b12c2789a9055b1aa04d16f8832afe35c24a1da
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-07-02 14:43:47 +0530

    Time: 7 ms (40.31%), Space: 14.7 MB (63.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // int st = 0;
-        // int end = nums.size() - 1;
-        // while(st<=end){
-        //     int currsum = nums[st] + nums[end];
-        //     if(abs(currsum) == abs(target)){   
-        //         return {st , end};
-        //     }
-        //     else if(abs(currsum) > abs(target)){
-        //         end--;
-        //     }else{
-        //         st++;
-        //     }   
-        // }
-        // return {st , end};
-    
         vector<pair<int , int>> arr;
         for(int i = 0 ; i< nums.size() ; i++){
             arr.push_back({nums[i] , i});
@@ -36,11 +21,6 @@ public:
             }
         }
         return {};
-
-        
-
-
-
 
     }
 };
