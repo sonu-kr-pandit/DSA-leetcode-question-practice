26f66b400a1cad0f8bcea168bf7d13b6651b8590
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-07-03 16:01:35 +0530

    Time: 51 ms (44.13%), Space: 29.2 MB (30.53%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin() , nums.end());
         vector<vector<int>> ans;
-        for(int i  = 0 ; i< nums.size() ; i++){
+        for(int i  = 0 ; i< nums.size()-2 ; i++){
             int st = i+1;
             int end = nums.size() - 1;
             if(i > 0 && nums[i] == nums[i-1]) continue;
