294e96ef5a246d5d059b92a60b5fde43422d79d4
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-07-03 18:12:59 +0530

    Time: 0 ms (100%), Space: 23.7 MB (82.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    vector<int> swift(vector<int>& nums, int i) {
-        while (i != nums.size() - 1) {
-            swap(nums[i], nums[i + 1]);
-            i++;
-        }
-        return nums;
-    }
     void moveZeroes(vector<int>& nums) {
-        for (int i = nums.size()-1; i >=0; i--) {
-            if (nums[i] == 0) {
-                swift(nums, i);
+        int pos = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[pos]);
+                pos++;
             }
         }
     }
