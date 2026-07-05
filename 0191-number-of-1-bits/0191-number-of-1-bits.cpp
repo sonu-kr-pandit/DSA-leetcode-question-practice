8662c641591f4a58a5f33a22ea743d839367288e
sonu-kr-pandit <sonukumarpandit00000000@gmail.com>
2026-07-06 00:35:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (47.95%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int hammingWeight(int n) {
         int count = 0;
+        // while(n>0){
+        //     if(!(n&1)){
+        //         // count++;
+        //         n=n>>1;
+        //     }else{
+        //         count++;
+        //         n=n>>1;
+        //     }
+        // }
         while(n>0){
-            if(!(n&1)){
-                // count++;
-                n=n>>1;
-            }else{
-                count++;
-                n=n>>1;
-            }
+            int lg = n&1;
+            count+=lg;
+            n = n>>1;
         }
         return count;
         
