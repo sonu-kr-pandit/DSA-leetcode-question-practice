da0308c9ef2c216a215ace85098737eba509abd6
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-07-08 13:31:15 +0530

    Time: 3 ms (23.35%), Space: 10.5 MB (79.38%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1 ,0);
+        for(int i = 0 ; i<n+1;i++){
+            ans[i] = __builtin_popcount(i);
+        }
+        return ans;
+        
+        
+    }
+};
