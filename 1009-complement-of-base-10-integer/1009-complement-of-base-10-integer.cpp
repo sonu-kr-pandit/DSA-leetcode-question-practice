6e8a160319ebfc8b32ff4a63850f3756a2613232
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-07-08 14:34:46 +0530

    Time: 0 ms (100%), Space: 8 MB (4.44%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0){return 1;}
+
+        int count = 0;
+        int ans = 0;
+        while (n) {
+            int r = n % 2;
+            ans = ans + (pow(2,count)*!r);
+            n = n / 2;
+            count++;
+        }
+        return ans;
+    }
+};
