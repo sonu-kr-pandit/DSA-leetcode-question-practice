52e0a60d1f7de808f4c42c3cb447f22a129c20a3
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-07-10 13:14:08 +0530

    Time: 11 ms (38.04%), Space: 7.8 MB (38.1%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,6 +3,7 @@ public:
     int fibonnaci(int n ){
         if(n==0 | n==1){return n;};
         return fibonnaci(n-1)+fibonnaci(n-2);
+        
     }
     int fib(int n) {
         return fibonnaci(n);
