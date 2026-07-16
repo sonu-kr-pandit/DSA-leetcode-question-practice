7b04ac267fd6961ce01ed3b5ad34477a59e6bef1
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-07-16 16:03:42 +0530

    Time: 658 ms (23.09%), Space: 295.8 MB (9.15%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    void merge(vector<int> &nums, int si , int mid , int ei){
+        vector<int> temp;
+        int i =si , j = mid+1;
+        while(i<=mid && j<=ei ){
+            if(nums[i]<=nums[j]){
+                temp.push_back(nums[i++]);
+            }else{
+                temp.push_back(nums[j++]);
+            }
+        }
+        while(i<=mid){temp.push_back(nums[i++]);};
+        while(j<=ei){temp.push_back(nums[j++]);};
+        // for(int p =si , x =0 ; p<temp.size() ; p++){
+        //     nums[p] = temp[x++];
+        // }
+        for(int k = 0; k < temp.size(); k++){
+            nums[si + k] = temp[k];
+        }
+    }
+
+    void MergeSort(vector<int> &nums , int si , int ei){
+        if(si>=ei){return ;}
+        int mid = si + (ei-si)/2;    
+        MergeSort(nums , si , mid);
+        MergeSort(nums ,mid+1 ,ei);
+        merge(nums , si , mid , ei);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int si = 0 ;
+        int ei = nums.size()-1;
+        MergeSort(nums , si , ei);
+        return nums;
+        
+    }
+};
