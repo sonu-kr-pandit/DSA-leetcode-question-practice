6e2b336ffde38b11ba0dcbd4dcf22a39b8ca1dee
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-07-21 00:39:05 +0530

    Time: 0 ms (100%), Space: 18.2 MB (7.59%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int k) {
+          int n = nums.size();
+        for(int i = 0 ; i<= n-1; i++){      // linear TC
+            if(nums[i]==k){return true;}
+        }
+
+
+
+        // int st =0;                        // binay TC
+        // int end = n-1;
+        // while(st<=end){
+        //     int mid = st +(end-st)/2;
+        //     if(nums[mid]==k){
+        //         return true;
+        //     }
+        //     if(nums[st]<=nums[mid]){
+        //         if(nums[st]<= k && k<nums[mid] ){
+        //             end = mid- 1;
+        //         }else{
+        //             st = mid + 1;
+        //         }
+        //     }else{
+        //          if(nums[mid]< k && k <= nums[end]){
+        //             st = mid +1 ;
+        //         }else{
+        //             end = mid - 1;
+        //         }
+
+        //     }
+        // }
+        
+        return false;
+        
+    }
+};
