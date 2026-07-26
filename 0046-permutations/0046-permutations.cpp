d08eb0cd904fbd8efaf48809a2fc8f5b75fa200c
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-07-27 01:47:18 +0530

    Time: 3 ms (26.12%), Space: 11.4 MB (16.42%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+      unordered_set<int> s;
+    vector<vector<int>> ans;
+
+    void solve(vector<int>& nums, vector<int>& temp) {
+        if (temp.size() == nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.find(nums[i]) == s.end()) {
+                temp.push_back(nums[i]);
+                s.insert(nums[i]);
+                solve(nums, temp);
+                temp.pop_back();
+                s.erase(nums[i]);
+            }
+        }
+       
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int > temp;
+        solve(nums , temp);
+        return ans;
+        
+    }
+};
