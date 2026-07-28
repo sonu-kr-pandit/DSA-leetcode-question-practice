392370e859e25e8e48f5098027dbdf9f94964d6d
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-07-28 23:49:32 +0530

    Time: 1 ms (85.83%), Space: 13.2 MB (77.53%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     vector<string> ans;
-    bool isValid(string s){
-        int count = 0;
-        for(char &ch : s){
-            if(ch=='('){
-                count++;
-            }else{
-                count--;
-            }
-            if(count<0){return false;}
-        }
-        return count == 0;
-    }
+    // bool isValid(string s){
+    //     int count = 0;
+    //     for(char &ch : s){
+    //         if(ch=='('){
+    //             count++;
+    //         }else{
+    //             count--;
+    //         }
+    //         if(count<0){return false;}
+    //     }
+    //     return count == 0;
+    // }
 
     void solve(string &s  , int n , int lbracket , int rbracket){
         if(s.length() ==  2*n){
-            if(isValid(s)){
+            // if(isValid(s)){
                 ans.push_back(s);
-            }
+            // }
             return;
         }
 
