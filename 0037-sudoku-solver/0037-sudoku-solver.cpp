ce68e0a4541491450bff80f6a60d0fc6952e0d0d
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-07-31 03:36:32 +0530

    Time: 387 ms (29.74%), Space: 8.7 MB (71.35%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+
+    bool isValid(int r , int c , int dgt , vector<vector<char>>& board){
+        // vertically
+        for(int i = 0 ; i<9 ; i++){
+            if(board[i][c]==dgt){return false;}
+             if(board[r][i]==dgt){return false;}
+        }
+
+        // horizontal
+        // for(int i = 0 ; i<9 ; i++){
+        //     if(board[r][i]==dgt){return false;}
+        // }
+
+        // 3x3 grid board
+        int strow = (r/3)*3;
+        int stcol = (c/3)*3;
+        for(int i = strow ; i<=strow + 2 ; i++){
+            for(int j = stcol ; j<=stcol+2 ; j++){
+                if(board[i][j]==dgt){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+
+
+    bool solution(vector<vector<char>>& board){
+        for(int i = 0 ; i < 9 ; i++){
+            for(int j = 0 ; j < 9 ; j++){
+                if(board[i][j] == '.'){
+                    for(char d = '1' ; d<= '9' ; d++){
+                        if(isValid(i,j,d,board)){
+                            board[i][j] = d;
+                            if(solution(board)==true){
+                                return true;
+                            }
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solution(board);
+        
+    }
+};
