0792e48647f0d46cca164dcab1a13cd0c1d458f8
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-07-31 01:29:15 +0530

    Time: 10 ms (13.65%), Space: 12.2 MB (18.47%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,51 +1,87 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
+    // method 1
+    // vector<vector<string>> ans;
+    // bool isValid(int row , int col , vector<string> &board , int n ){
+    //     // upword
+    //     for(int i = row-1 ; i >=0; i-- ){
+    //         if(board[i][col]=='Q'){
+    //             return false;
+    //         }
+    //     }        //digonally upward in right
+    //     for(int i = row-1 , j =  col+1; i >=0 && j<n; i--,j++ ){
+    //         if(board[i][j]=='Q'){
+    //             return false;
+    //         }
+    //     }
+    //     //digonally upward in left
+    //      for(int i = row-1 , j =  col-1; i >=0 && j>=0; i--,j-- ){
+    //         if(board[i][j]=='Q'){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    // void solution(int n , int row , vector<string> &board){
+    //     if(row>=n){
+    //         ans.push_back(board);
+    //         return;
+    //     }
+    //     for(int col = 0 ; col <n ; col++){
+    //         if(isValid(row , col, board ,  n)){
+    //             board[row][col] = 'Q';
+    //             solution(n , row+1 , board);
+    //             board[row][col] = '.';
+    //         }
+    //     }
 
-    bool isValid(int row , int col , vector<string> &board , int n ){
-        // upword
-        for(int i = row-1 ; i >=0; i-- ){
-            if(board[i][col]=='Q'){
-                return false;
-            }
-        }        //digonally upward in right
-        for(int i = row-1 , j =  col+1; i >=0 && j<n; i--,j++ ){
-            if(board[i][j]=='Q'){
-                return false;
-            }
-        }
-        //digonally upward in left
-         for(int i = row-1 , j =  col-1; i >=0 && j>=0; i--,j-- ){
-            if(board[i][j]=='Q'){
-                return false;
-            }
-        }
-        return true;
-    }
-    void solution(int n , int row , vector<string> &board){
-        if(row>=n){
+    // }
+    // vector<vector<string>> solveNQueens(int n) {
+    //     vector<string> board(n, string(n, '.'));
+    //     solution(n , 0 , board);
+    //     return ans;
+    // }
+
+    // method 2
+    vector<vector<string>> ans;
+    unordered_set<int> Dig;
+    unordered_set<int> A_Dig;
+    unordered_set<int> colm;
+
+    void solution(int n, int row, vector<string>& board) {
+
+        if (row >= n) {
             ans.push_back(board);
             return;
         }
-        for(int col = 0 ; col <n ; col++){
-            if(isValid(row , col, board ,  n)){
-                board[row][col] = 'Q';
-                solution(n , row+1 , board);
-                board[row][col] = '.';
 
+        for (int col = 0; col < n; col++) {
+
+            int Dconst = col + row;
+            int A_Dconst = row - col;
+
+            if (Dig.find(Dconst) != Dig.end() ||
+                A_Dig.find(A_Dconst) != A_Dig.end() ||
+                colm.find(col) != colm.end()) {
+                continue;
             }
-        }
 
+            Dig.insert(Dconst);
+            A_Dig.insert(A_Dconst);
+            colm.insert(col);
+            board[row][col] = 'Q';
+
+            solution(n, row + 1, board);
+
+            Dig.erase(Dconst);
+            A_Dig.erase(A_Dconst);
+            colm.erase(col);
+            board[row][col] = '.';
+        }
     }
     vector<vector<string>> solveNQueens(int n) {
-        // vector<vector<char>> board(n , vector<char> (n , '.'));
-        // vector<string> board(n , '.');
         vector<string> board(n, string(n, '.'));
-        solution(n , 0 , board);
-
+        solution(n, 0, board);
         return ans;
-        
-
-        
     }
 };
