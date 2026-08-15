942e5cbe9b0e7d76c2cddcc90a1fd379dc9768dc
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-08-15 16:39:08 +0530

    Time: 15 ms (17.55%), Space: 14.3 MB (49.39%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     stack<char> stk;
-    // string reverse(string s){
-    //     int i = 0 ; 
-    //     int j = s.size()-1;
-    //     while( i < j){
-    //         swap(s[i], s[j]);
-    //     }
-    //     return s;
-    // }
     string removeDuplicates(string s) {
         for(int i = s.size()-1 ; i >=0 ; i--){
             if(stk.empty() || stk.top() != s[i]){
@@ -21,9 +13,7 @@ public:
         while(!stk.empty()){
             ans += stk.top();
             stk.pop();
-
         }
-        
         return ans;
     }
 };
