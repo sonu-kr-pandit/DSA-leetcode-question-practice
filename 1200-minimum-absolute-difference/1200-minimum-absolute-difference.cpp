71f62b140495603935cabac21494f470eb167b67
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-08-18 15:01:07 +0530

    Time: 15 ms (75.82%), Space: 36.9 MB (10.79%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -8,25 +8,14 @@ public:
         for (int i = 0; i < n - 1; i++) {
             int diff = abs(arr[i + 1] - arr[i]);
             if (ans.empty()) {
-                // vector<int> temp;
-                // temp.push_back(arr[i]);
-                // temp.push_back(arr[i + 1]);
                 ans.push_back({arr[i] , arr[i+1]});
                 minCmp = diff;
             } else {
                 if (diff < minCmp) {
                     ans.clear();
-                    // vector<int> temp;
-                    // temp.push_back(arr[i]);
-                    // temp.push_back(arr[i + 1]);
-                    // ans.push_back(temp);
                      ans.push_back({arr[i] , arr[i+1]});
                     minCmp = diff;
                 } else if (diff == minCmp) {
-                    // vector<int> temp;
-                    // temp.push_back(arr[i]);
-                    // temp.push_back(arr[i + 1]);
-                    // ans.push_back(temp);
                      ans.push_back({arr[i] , arr[i+1]});
                 }
             }
