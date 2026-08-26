2e103b67241d6a1fe8212bd670bd82d61da6bd78
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-08-26 20:30:12 +0530

    Time: 0 ms (100%), Space: 12.8 MB (47.92%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -21,9 +21,9 @@ public:
         // }
         // return isSameTree(p->left , q->left) && isSameTree(p->right , q->right);
 
-        if(p == NULL && q == NULL) return true;      // both empty -> same
-        if(p == NULL || q == NULL) return false;     // one empty, other not -> different
-        if(p->val != q->val) return false;            // now safe to dereference
+        if(p == NULL && q == NULL) return true;      
+        if(p == NULL || q == NULL) return false;     
+        if(p->val != q->val) return false;          
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
         
     }
