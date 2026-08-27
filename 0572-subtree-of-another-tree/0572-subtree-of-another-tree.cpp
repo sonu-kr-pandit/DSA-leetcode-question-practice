1d81f8c8cc8157f6425bce22750b5f240100e0c1
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-08-27 12:05:16 +0530

    Time: 0 ms (100%), Space: 29.1 MB (38.48%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -19,8 +19,23 @@ public:
         return solve(p->left, q->left) && solve(p->right, q->right);
         
     }
-    bool solution( TreeNode* root, TreeNode* subRoot ){
-        if(root!= NULL && subRoot != NULL){
+    // bool solution( TreeNode* root, TreeNode* subRoot ){
+    //     if(root!= NULL && subRoot != NULL){
+    //          if(root->val == subRoot->val){
+    //             if(solve(root->left , subRoot->left) && solve(root->right , subRoot->right)){
+    //                 return true;
+    //             }
+    //          }
+    //     }
+    //     if(root == NULL || subRoot == NULL){
+    //         return false; 
+    //     }
+    //     return solution(root->left , subRoot) || solution(root->right , subRoot);
+    // }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+
+         if(root!= NULL && subRoot != NULL){
              if(root->val == subRoot->val){
                 if(solve(root->left , subRoot->left) && solve(root->right , subRoot->right)){
                     return true;
@@ -30,10 +45,7 @@ public:
         if(root == NULL || subRoot == NULL){
             return false; 
         }
-        return solution(root->left , subRoot) || solution(root->right , subRoot);
-    }
-
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        return solution(root , subRoot);
+        return isSubtree(root->left , subRoot) || isSubtree(root->right , subRoot);
+        // return solution(root , subRoot);
     }
 };
