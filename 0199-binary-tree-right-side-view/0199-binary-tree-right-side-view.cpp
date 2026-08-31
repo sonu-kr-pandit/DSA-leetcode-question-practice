18a9b87bcc5b18540f2ae47928c265a7a19ee53d
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-08-31 19:19:56 +0530

    Time: 0 ms (100%), Space: 15.1 MB (40.56%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -13,45 +13,45 @@
 class Solution {
 public:
 
-    void preOrder(TreeNode* root , int level , vector<int> &ans){
-        if(root == NULL){return ;}
+    // void preOrder(TreeNode* root , int level , vector<int> &ans){
+    //     if(root == NULL){return ;}
 
-        if(ans.size() < level){
-            ans.push_back(root->val);
-        }
+    //     if(ans.size() < level){
+    //         ans.push_back(root->val);
+    //     }
 
-        preOrder(root->right, level + 1 , ans);
-        preOrder(root->left , level + 1 , ans);
-    }
+    //     preOrder(root->right, level + 1 , ans);
+    //     preOrder(root->left , level + 1 , ans);
+    // }
 
     vector<int> rightSideView(TreeNode* root) {
 
         //** METHOD 1 , LEVEL ORDER TRAVERSAL (BFS) **//
 
-        // if(root == NULL){return {};}
-        // queue<TreeNode*> q;
-        // q.push(root);
-        // vector<int> ans;
-        // while (!q.empty()) {
-        //     int s = q.size();
-        //     TreeNode* node = NULL;
-        //     while (s--) {
-        //         node = q.front();
-        //         q.pop();
-        //         if(node->left != NULL) q.push(node->left);
-        //         if(node->right != NULL) q.push(node->right);
+        if(root == NULL){return {};}
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<int> ans;
+        while (!q.empty()) {
+            int s = q.size();
+            TreeNode* node = NULL;
+            while (s--) {
+                node = q.front();
+                q.pop();
+                if(node->left != NULL) q.push(node->left);
+                if(node->right != NULL) q.push(node->right);
                 
-        //     }
-        //     ans.push_back(node->val);
-        // }
-        // return ans;
+            }
+            ans.push_back(node->val);
+        }
+        return ans;
 
 
         //** METHOD 2 , PREORDER (DFS) **//
-        if(root == NULL){return {};}
+        // if(root == NULL){return {};}
 
-        vector<int> ans;
-        preOrder(root , 1 , ans);
-        return ans;
+        // vector<int> ans;
+        // preOrder(root , 1 , ans);
+        // return ans;
     }
 };
