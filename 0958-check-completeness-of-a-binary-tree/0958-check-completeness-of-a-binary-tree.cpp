eb8bd98d99c2f2b1ebd112ae19ac56156c190e66
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-09-01 23:23:43 +0530

    Time: 0 ms (100%), Space: 13.9 MB (32.08%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -12,17 +12,17 @@
 class Solution {
 public:
 
-    int count(TreeNode* root){
-        if(root == NULL ){return 0;}
-        return  1 + count(root->left) + count(root->right);
-    }
+    // int count(TreeNode* root){
+    //     if(root == NULL ){return 0;}
+    //     return  1 + count(root->left) + count(root->right);
+    // }
 
-    bool DFS(TreeNode* root , int i , int nodeCount){
-        if(root == NULL){return true;}
-        if(i > nodeCount){return false;}
+    // bool DFS(TreeNode* root , int i , int nodeCount){
+    //     if(root == NULL){return true;}
+    //     if(i > nodeCount){return false;}
 
-        return DFS(root->left , 2*i , nodeCount) && DFS(root->right , 2*i + 1 , nodeCount);
-    }
+    //     return DFS(root->left , 2*i , nodeCount) && DFS(root->right , 2*i + 1 , nodeCount);
+    // }
 
 
 
@@ -31,40 +31,40 @@ public:
 
         //*** method 1 , BFS ***//
 
-        // if(root == NULL){return false;}
+        if(root == NULL){return false;}
 
-        // queue<TreeNode*> q;
+        queue<TreeNode*> q;
 
-        // q.push(root);
+        q.push(root);
 
-        // bool pastNull = false;
+        bool pastNull = false;
 
-        // while(!q.empty()){
-        //     TreeNode* node = q.front();
-        //     q.pop();
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
 
-        //     if(node == NULL){
-        //         pastNull = true;
-        //     }else{
-        //         if(pastNull == true){
-        //             return false;
-        //         }
-        //         q.push(node->left);
-        //         q.push(node->right);
-        //     }
-        // }
-        // return true;
+            if(node == NULL){
+                pastNull = true;
+            }else{
+                if(pastNull == true){
+                    return false;
+                }
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        return true;
 
 
         //*** METHOD 2 , DFS ***//
 
-        if(root == NULL) return false;
+        // if(root == NULL) return false;
 
-        int nodeCount = count(root);
+        // int nodeCount = count(root);
 
-        int i  = 1;
+        // int i  = 1;
 
-        return DFS(root , i , nodeCount);
+        // return DFS(root , i , nodeCount);
 
         
     }
