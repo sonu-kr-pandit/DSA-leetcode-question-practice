b945980ed6b6727de351591a083b647e3daac1cc
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-09-03 20:47:49 +0530

    Time: 0 ms (100%), Space: 21.5 MB (67.25%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -14,7 +14,6 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(root == NULL) return false;
         targetSum = targetSum - root->val;
-        // if(targetSum < 0) return false;
         if(root->left == NULL && root->right == NULL && targetSum == 0 ) return true;
         return hasPathSum(root->left , targetSum ) ||  hasPathSum(root->right , targetSum ) ;
         
