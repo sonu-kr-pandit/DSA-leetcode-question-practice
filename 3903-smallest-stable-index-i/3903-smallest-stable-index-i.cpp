b99f3c7457255c35ab84592da583b8fef6a7987e
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-09-04 15:24:07 +0530

    Time: 0 ms (100%), Space: 31.1 MB (56.73%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
-
     int solution(vector<int> nums , int i ){
         int maxIS = INT_MIN;
         int minIS = INT_MAX;
         for(int j = 0 ; j <=i ; j++){
             maxIS = max(maxIS , nums[j]);
         }
-
         for(int j = i ; j < nums.size() ; j++){
             minIS = min(minIS , nums[j]);
         }
         return maxIS - minIS ;
     }
-
     int firstStableIndex(vector<int>& nums, int k) {
-        vector<int> ans;
-        for(int i = 0 ; i< nums.size();  i++){
-            int IS = solution(nums , i);
-            ans.push_back(IS);
+
+        // method 1 ,TC = O(N^2); brute force method
+        // vector<int> ans;
+        // for(int i = 0 ; i< nums.size();  i++){
+        //     int IS = solution(nums , i);
+        //     ans.push_back(IS);
+        // }
+        // for(int i  = 0; i < ans.size() ; i++){
+        //     if(ans[i] <= k ){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // method 2 , TC = O(N) ;
+
+        vector<int> minEL(nums.size());
+        int minIND = INT_MAX;
+        for(int i = nums.size()-1 ; i>= 0 ; i--){
+            minIND = min(nums[i] , minIND);
+            minEL[i] = minIND;
         }
-        // int res = INT_MAX;
-        for(int i  = 0; i < ans.size() ; i++){
-            if(ans[i] <= k ){
-                return i;
-            }
+
+        int Max = INT_MIN;
+
+        for(int i = 0 ; i< nums.size() ; i++){
+            Max = max(Max , nums[i]);
+            int Min = minEL[i];
+            if(Max - Min <= k) return i;
         }
-
-
-
         return -1;
     }
 };
