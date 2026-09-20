48bacfc29b1e3c0f15a033c7b94ea63f1f5f1108
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-09-20 11:22:32 +0530

    Time: 65 ms (7.92%), Space: 75.8 MB (6.97%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,8 +1,55 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin() , nums.end());
-        return nums[nums.size()-k];
-        
+    int part(vector<int>& nums, int si, int ei) {
+        int randomIdx = si + rand() % (ei - si + 1);
+        swap(nums[randomIdx], nums[ei]);
+        int pivot = nums[ei];
+        int i = si - 1;
+        for (int j = 0; j < ei; j++) {
+            if (nums[j] <= pivot) {
+                i++;
+                swap(nums[i], nums[j]);
+            }
+        }
+        i++;
+        swap(nums[i], nums[ei]);
+        return i;
     }
+    int findKthLargest(vector<int>& nums, int k) {
+        // int n = nums.size();
+        // int si = 0;
+        // int ei = n-1;
+        // int tgt = n-k;
+        // while(si<=ei){
+        //     int pidx = part(nums , si , ei);
+        //     if(pidx==k-1){
+        //         nums[pidx];
+        //     }
+        //     else if(pidx>tgt){
+        //        ei = pidx-1;
+        //     }else{
+        //         ei=pidx+1;
+        //     }
+        // }
+        // return -1;
+        priority_queue<int> pq;
+        for (int& num : nums) {
+            pq.push(num);
+        }
+        while (k != 1) {
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+
+    // priority_queue<int> pq;
+    // for(int &num : nums){
+    //     pq.push(num);
+    // }
+    // while( k!=1){
+    //     pq.pop();
+    //     k--;
+    // }
+    // return pq.top();
 };
