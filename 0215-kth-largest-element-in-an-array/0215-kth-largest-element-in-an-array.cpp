c0ee27b5b69dc029422320c119c969f06bfd7641
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-09-20 11:22:54 +0530

    Time: 43 ms (34.36%), Space: 75.5 MB (17.36%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int part(vector<int>& nums, int si, int ei) {
-        int randomIdx = si + rand() % (ei - si + 1);
-        swap(nums[randomIdx], nums[ei]);
-        int pivot = nums[ei];
-        int i = si - 1;
-        for (int j = 0; j < ei; j++) {
-            if (nums[j] <= pivot) {
-                i++;
-                swap(nums[i], nums[j]);
-            }
-        }
-        i++;
-        swap(nums[i], nums[ei]);
-        return i;
-    }
+    // int part(vector<int>& nums, int si, int ei) {
+    //     int randomIdx = si + rand() % (ei - si + 1);
+    //     swap(nums[randomIdx], nums[ei]);
+    //     int pivot = nums[ei];
+    //     int i = si - 1;
+    //     for (int j = 0; j < ei; j++) {
+    //         if (nums[j] <= pivot) {
+    //             i++;
+    //             swap(nums[i], nums[j]);
+    //         }
+    //     }
+    //     i++;
+    //     swap(nums[i], nums[ei]);
+    //     return i;
+    // }
     int findKthLargest(vector<int>& nums, int k) {
         // int n = nums.size();
         // int si = 0;
