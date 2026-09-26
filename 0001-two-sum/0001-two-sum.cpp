fb06eabcfd40f442ff51728b3bc1d37e7064ed4a
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-09-26 09:58:12 +0530

    Time: 3 ms (67.88%), Space: 15 MB (12.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int , int>> arr;
-        for(int i = 0 ; i< nums.size() ; i++){
-            arr.push_back({nums[i] , i});
-        }
-        sort(arr.begin() , arr.end());
-        int st = 0 ; 
-        int end = arr.size()-1;
-        while(st<=end){
-            int currsum = arr[st].first + arr[end].first ; 
-            if(currsum == target){
-                return {arr[st].second ,arr[end].second};
-            }
-            else if(currsum > target){
-                end--;
-            }
-            else{
-                st++;
-            }
-        }
-        return {};
+        // vector<pair<int , int>> arr;
+        // for(int i = 0 ; i< nums.size() ; i++){
+        //     arr.push_back({nums[i] , i});
+        // }
+        // sort(arr.begin() , arr.end());
+        // int st = 0 ;
+        // int end = arr.size()-1;
+        // while(st<=end){
+        //     int currsum = arr[st].first + arr[end].first ;
+        //     if(currsum == target){
+        //         return {arr[st].second ,arr[end].second};
+        //     }
+        //     else if(currsum > target){
+        //         end--;
+        //     }
+        //     else{
+        //         st++;
+        //     }
+        // }
+        // return {};
 
+            int n = nums.size();
+            unordered_map<int, int> mp;
+
+            for (int i = 0; i < n; i++) {
+                int comp = target - nums[i];
+
+                if (mp.find(comp) != mp.end()) {
+                    return {mp[comp], i};
+                }
+
+                mp[nums[i]] = i;
+            }
+
+            return {};
+        
     }
 };
