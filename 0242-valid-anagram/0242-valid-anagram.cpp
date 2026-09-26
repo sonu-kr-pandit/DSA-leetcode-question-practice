30fa22202d12c615a60373b27c85a9bd5aabefac
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-09-26 17:52:17 +0530

    Time: 7 ms (18.09%), Space: 9.7 MB (74.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(t.length() != s.size()){return false;}
-        int freq[26] = { 0 };
-        for(int i = 0 ; i< s.size(); i++){
-            freq[s[i]-'a']++;
+        if (t.length() != s.size()) {
+            return false;
         }
-        for(int i = 0 ; i< t.size(); i++){
-           int idx = t[i] - 'a';
-           if(freq[idx]==0){return false;}
-           freq[idx]--;
+
+        // int freq[26] = { 0 };
+        // for(int i = 0 ; i< s.size(); i++){
+        //     freq[s[i]-'a']++;
+        // }
+        // for(int i = 0 ; i< t.size(); i++){
+        //    int idx = t[i] - 'a';
+        //    if(freq[idx]==0){return false;}
+        //    freq[idx]--;
+        // }
+        // return true;
+
+        unordered_map<char, int> mp;
+
+        for (char ch : s)
+            mp[ch]++;
+
+        for (char ch : t) {
+            mp[ch]--;
+            if (mp[ch] < 0)
+                return false;
         }
+
         return true;
     }
 };
