150aef5016537a6b36ca5e4815d0be44bf1eef2b
Sonu Kumar Pandit <sonukumarpandit00000000@gmail.com>
2026-09-26 18:18:31 +0530

    Time: 71 ms (60.54%), Space: 111.3 MB (63.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,14 +2,23 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin() , nums.end());
+        // sort(nums.begin() , nums.end());                    // nlog(n)  tc
 
-        for(int i = 0 ; i< n-1 ;i++){
-            if(nums[i]==nums[i+1]){return true;}
+        // for(int i = 0 ; i< n-1 ;i++){
+        //     if(nums[i]==nums[i+1]){return true;}
+        // }
+
+        // return false;
+
+        unordered_map<int, int> mp;                             // n tc
+
+        for (int i = 0; i < n; i++) {
+            if (mp.find(nums[i]) != mp.end()) {
+                return true;
+            }
+            mp[nums[i]] = 1;
         }
-        
+
         return false;
-      
-        
     }
 };
